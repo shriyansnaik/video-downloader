8e85441e7e366c5a5e9568a1812d01fbba415584
Shriyans Naik <66057990+shriyansnaik@users.noreply.github.com>
2024-08-24 11:59:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_available_formats(video_url):
         'quiet': True,
         'noplaylist': True,
         'listformats': True,
-        'cookies_from_browser': 'safari'
+        'cookiesfrombrowser': ('safari',)
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(video_url, download=False)
